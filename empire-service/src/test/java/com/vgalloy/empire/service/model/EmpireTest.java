1b1c7efbc47116beacae1a20712400a9c92dffac
Vincent Galloy <vgalloy@excilys.com>
2017-08-20 18:46:31 +0200

    Update dependencies

---
+++ empire-service/src/test/java/com/vgalloy/empire/service/model/EmpireTest.java
@@ -0,0 +1,25 @@
+package com.vgalloy.empire.service.model;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Created by Vincent Galloy on 20/08/17.
+ *
+ * @author Vincent Galloy
+ */
+public final class EmpireTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void simple() {
+        // EXCEPTION
+        expectedException.expect(NullPointerException.class);
+
+        // WHEN
+        Empire.newInstance(null);
+    }
+}
