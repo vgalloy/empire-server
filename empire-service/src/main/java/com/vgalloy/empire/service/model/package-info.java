77d16d28ff1ad9caf24c5e142fd13070fc158136
Vincent Galloy <vgalloy@excilys.com>
2017-08-20 19:47:02 +0200

    Update jenkins file

--- empire-service/src/main/java/com/vgalloy/empire/service/model/package-info.java
+++ empire-service/src/main/java/com/vgalloy/empire/service/model/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Create by Vincent Galloy on 02/08/2017.
- * All the class of this package represents business model. They should not be serializable directly.
- * They can hold an internal logic and can't hold null field without explicitly declare it with an
+ * All the classes of this (sub)package represents business model. They should not be serializable directly.
+ * They can hold an internal logic but can't hold null field without explicitly declare it with an
  * {@link java.util.Optional}.
  *
  * @author Vincent Galloy
