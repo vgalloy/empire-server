f4b42384ed2c93b43c4af8da2cecf05adb443496
Vincent Galloy <vgalloy@excilys.com>
2018-03-11 18:03:42 +0100

    Empire use now a builder

--- empire-service/src/main/java/com/vgalloy/empire/service/model/Empire.java
+++ empire-service/src/main/java/com/vgalloy/empire/service/model/Empire.java
@@ -23,51 +23,20 @@ public final class Empire {
      * Constructor.
      * Private to avoid non managed instantiation.
      *
-     * @param empireId           the empireId
-     * @param playerInstructions the playerInstructions
-     * @param round              the round
-     * @param population         the population
-     * @param gold               the gold
-     * @param tax                the tax
-     * @param stock              the stock
+     * @param builder the builder
      */
-    private Empire(EmpireId empireId, PlayerInstructions playerInstructions, Round round, long population, long gold, long tax, Stock stock) {
-        this.empireId = Objects.requireNonNull(empireId);
-        this.playerInstructions = Objects.requireNonNull(playerInstructions);
-        this.round = Objects.requireNonNull(round);
-        this.population = population;
-        this.gold = gold;
-        this.tax = tax;
-        this.stock = Objects.requireNonNull(stock);
-    }
+    private Empire(Builder builder) {
+        this.empireId = Objects.requireNonNull(builder.empireId);
+        this.playerInstructions = Objects.requireNonNull(builder.playerInstructions);
+        this.round = Objects.requireNonNull(builder.round);
+        this.population = builder.population;
+        this.gold = builder.gold;
+        this.tax = builder.tax;
+        this.stock = Objects.requireNonNull(builder.stock);
 
-    /**
-     * Build a new instance of Empire.
-     *
-     * @return the new Empire
-     */
-    public static Empire newInstance() {
-        return internalCreate(EmpireId.newInstance(), PlayerInstructions.newEmpty(), Round.newInstance(), 100L, 0L, 100L, Stock.newStock());
-    }
-
-    /**
-     * Constructor.
-     *
-     * @param empireId           the empireId
-     * @param playerInstructions the playerInstructions
-     * @param round              the round
-     * @param population         the population
-     * @param gold               the gold
-     * @param tax                the tax
-     * @param stock              the stock
-     * @return a new instance of empire
-     */
-    private static Empire internalCreate(EmpireId empireId, PlayerInstructions playerInstructions, Round round, long population, long gold, long tax, Stock stock) {
         Assert.state(gold >= 0, "Empire gold can't be negative");
         Assert.state(population >= 0, "Empire population can't be negative");
         Assert.state(tax >= 0, "Empire tax can't be negative");
-
-        return new Empire(empireId, playerInstructions, round, population, gold, tax, stock);
     }
 
     public EmpireId getEmpireId() {
@@ -105,7 +74,9 @@ public final class Empire {
      * @return the new empire
      */
     public Empire playerInstructions(PlayerInstructions playerInstructions) {
-        return internalCreate(empireId, playerInstructions, round, population, gold, tax, stock);
+        Builder builder = Builder.from(this);
+        builder.playerInstructions = playerInstructions;
+        return builder.build();
     }
 
     /**
@@ -115,7 +86,9 @@ public final class Empire {
      * @return the new empire
      */
     public Empire round(Round round) {
-        return internalCreate(empireId, playerInstructions, round, population, gold, tax, stock);
+        Builder builder = Builder.from(this);
+        builder.round = round;
+        return builder.build();
     }
 
     /**
@@ -125,7 +98,9 @@ public final class Empire {
      * @return the new empire
      */
     public Empire population(long population) {
-        return internalCreate(empireId, playerInstructions, round, population, gold, tax, stock);
+        Builder builder = Builder.from(this);
+        builder.population = population;
+        return builder.build();
     }
 
     /**
@@ -135,7 +110,9 @@ public final class Empire {
      * @return the new empire
      */
     public Empire gold(long gold) {
-        return internalCreate(empireId, playerInstructions, round, population, gold, tax, stock);
+        Builder builder = Builder.from(this);
+        builder.gold = gold;
+        return builder.build();
     }
 
     /**
@@ -145,7 +122,9 @@ public final class Empire {
      * @return the new empire
      */
     public Empire tax(long tax) {
-        return internalCreate(empireId, playerInstructions, round, population, gold, tax, stock);
+        Builder builder = Builder.from(this);
+        builder.tax = tax;
+        return builder.build();
     }
 
     /**
@@ -155,6 +134,41 @@ public final class Empire {
      * @return the new empire
      */
     public Empire stock(Stock stock) {
-        return internalCreate(empireId, playerInstructions, round, population, gold, tax, stock);
+        Builder builder = Builder.from(this);
+        builder.stock = stock;
+        return builder.build();
+    }
+
+    private static class Builder {
+        private EmpireId empireId;
+        private PlayerInstructions playerInstructions;
+        private Round round;
+        private long population;
+        private long gold;
+        private long tax;
+        private Stock stock;
+
+        /**
+         * Constructor.
+         *
+         * @return a new instance of empire
+         */
+        private static Builder from(Empire empire) {
+            Builder builder = new Builder();
+
+            builder.empireId = empire.getEmpireId();
+            builder.playerInstructions = empire.getPlayerInstructions();
+            builder.round = empire.getRound();
+            builder.population = empire.getPopulation();
+            builder.gold = empire.getGold();
+            builder.tax = empire.getTax();
+            builder.stock = empire.getStock();
+
+            return builder;
+        }
+
+        private Empire build() {
+            return new Empire(this);
+        }
     }
 }
