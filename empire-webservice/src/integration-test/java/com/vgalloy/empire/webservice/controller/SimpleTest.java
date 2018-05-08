4087be058943f6d0b28ffab786d5483474e7dbea
Vincent Galloy <galloy.v@gmail.com>
2018-05-08 17:34:04 +0200

    Add error 404 test

---
+++ empire-webservice/src/integration-test/java/com/vgalloy/empire/webservice/controller/SimpleTest.java
@@ -0,0 +1,43 @@
+package com.vgalloy.empire.webservice.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.vgalloy.empire.webservice.ServerConfig;
+import com.vgalloy.empire.webservice.dto.ErrorDto;
+
+/**
+ * Created by Vincent Galloy on 08/05/18.
+ *
+ * @author Vincent Galloy
+ */
+@RunWith(SpringRunner.class)
+@Import(ServerConfig.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SimpleTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void error404() {
+        // GIVEN
+        String nonExistingUrl = "/azeaz";
+
+        // WHEN
+        ResponseEntity<ErrorDto> result = restTemplate.getForEntity(nonExistingUrl, ErrorDto.class);
+
+        // THEN
+        Assert.assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+        Assert.assertEquals(HttpStatus.NOT_FOUND.value(), result.getBody().getCode());
+        Assert.assertEquals(HttpStatus.NOT_FOUND.getReasonPhrase(), result.getBody().getMessage());
+    }
+}
