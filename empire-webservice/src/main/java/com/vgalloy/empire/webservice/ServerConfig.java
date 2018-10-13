b76dd4739070a6ec5c3a4e2b8f6d526fb3706f7e
Vincent Galloy <galloy.v@gmail.com>
2018-10-13 11:20:16 +0200

    Add feature package to web service

--- empire-webservice/src/main/java/com/vgalloy/empire/webservice/ServerConfig.java
+++ empire-webservice/src/main/java/com/vgalloy/empire/webservice/ServerConfig.java
@@ -3,7 +3,13 @@ package com.vgalloy.empire.webservice;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication(scanBasePackages = "com.vgalloy.empire")
+@SpringBootApplication(scanBasePackages = {
+    "com.vgalloy.empire.webservice",
+    "com.vgalloy.empire.common",
+    "com.vgalloy.empire.service",
+    "com.vgalloy.empire.persistence",
+    "com.vgalloy.empire.i18n"
+})
 public class ServerConfig {
 
     /**
--- empire-webservice/src/main/java/com/vgalloy/empire/webservice/config/SecurityConfiguration.java
+++ empire-webservice/src/main/java/com/vgalloy/empire/webservice/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.vgalloy.empire.webservice.config;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,7 +18,6 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationEn
  *
  * @author Vincent Galloy
  */
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
