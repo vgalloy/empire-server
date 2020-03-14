3c618807f09685c3b69ee6c7a320d9c6ad0ca301
Vincent Galloy <galloy.v@gmail.com>
2020-03-14 11:06:12 +0100

    Clean up

--- empire-webservice/src/integration-test/java/com/vgalloy/empire/webservice/controller/UserControllerTest.java
+++ empire-webservice/src/integration-test/java/com/vgalloy/empire/webservice/controller/UserControllerTest.java
@@ -3,8 +3,14 @@ package com.vgalloy.empire.webservice.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vgalloy.empire.webservice.ServerConfig;
 import com.vgalloy.empire.webservice.dto.UserDto;
-import com.vgalloy.empire.webservice.resource.ResourceData;
 import com.vgalloy.empire.webservice.resource.ResourceList;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,14 +24,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Created by Vincent Galloy on 21/10/18.
  *
@@ -33,14 +31,12 @@ import java.util.regex.Pattern;
  */
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
-        classes = ServerConfig.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+    classes = ServerConfig.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
 
-  @LocalServerPort
-  private int port;
-  @Autowired
-  private TestRestTemplate restTemplate;
+  @LocalServerPort private int port;
+  @Autowired private TestRestTemplate restTemplate;
 
   @Test
   void getAllMustBeEmpty() {
@@ -48,9 +44,10 @@ public class UserControllerTest {
     final var url = "/users";
 
     // WHEN
-    @SuppressWarnings("unchecked") final ResponseEntity<ResourceList<UserDto>> result =
-            (ResponseEntity<ResourceList<UserDto>>)
-                    (ResponseEntity<?>) restTemplate.getForEntity(url, ResourceList.class);
+    @SuppressWarnings("unchecked")
+    final ResponseEntity<ResourceList<UserDto>> result =
+        (ResponseEntity<ResourceList<UserDto>>)
+            (ResponseEntity<?>) restTemplate.getForEntity(url, ResourceList.class);
 
     // THEN
     Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
@@ -61,29 +58,39 @@ public class UserControllerTest {
   @Test
   void createUser() throws Exception {
     // GIVEN
-    final UserDto user = new UserDto();
-    user.setLogin("login");
-    user.setPassword("password");
-
-    final HttpEntity<UserDto> httpEntity = new HttpEntity<>(user);
+    final HttpEntity<UserDto> httpEntity = userRequest();
 
     // WHEN
-    final ResponseEntity<String> result = restTemplate.exchange("/users/a91b1d64-aff6-4587-ac5c-6a58822b5b75", HttpMethod.PUT, httpEntity, String.class);
+    final ResponseEntity<String> result =
+        restTemplate.exchange(
+            "/users/a91b1d64-aff6-4587-ac5c-6a58822b5b75",
+            HttpMethod.PUT,
+            httpEntity,
+            String.class);
 
     // THEN
     final ObjectMapper mapper = new ObjectMapper();
     final String body = result.getBody();
-    final String expected = getResource("output.json")
+    final String expected =
+        getResource("output.json")
             .replaceAll("PORT", "" + port)
             .replaceAll("EXECUTION_TIME", "" + extractExecutionTime(body));
     Assertions.assertEquals(mapper.readTree(expected), mapper.readTree(body));
   }
 
+  private HttpEntity<UserDto> userRequest() {
+    final UserDto user = new UserDto();
+    user.setLogin("login");
+    user.setPassword("password");
+    return new HttpEntity<>(user);
+  }
+
   private static int extractExecutionTime(final String body) {
     final Matcher m = Pattern.compile("[0-9]{1,3}").matcher(body);
-    m.find();
-    final String ex = m.group();
-    return Integer.parseInt(ex);
+    if (m.find()) {
+      return Integer.parseInt(m.group());
+    }
+    throw new IllegalStateException("No match");
   }
 
   private static String getResource(final String resourceName) {
